887dde9c2574cc3a3f5e3ab7fc806dbec2d1c05f
Shafaet-j <lbshaft01@gmail.com>
2023-08-25 22:43:32 +0600

    navbar design done

--- src/pages/shared/navbar/Navbar.tsx
+++ src/pages/shared/navbar/Navbar.tsx
@@ -1,9 +1,78 @@
-const Navbar = () => {
-    return (
-        <div className=''>
-            Navbar
+import React from "react";
+import logo from "../../../assets/logo.png";
+import { Link, NavLink } from "react-router-dom";
+
+interface NavLink {
+  path: string;
+  title: string;
+}
+
+const Navbar: React.FC = () => {
+  const navlinks: NavLink[] = [
+    {
+      path: "/",
+      title: "Home",
+    },
+    {
+      path: "/aboutUs",
+      title: "About Us",
+    },
+    {
+      path: "/services",
+      title: "Services",
+    },
+    {
+      path: "/doctors",
+      title: "Doctors",
+    },
+    {
+      path: "/gallery",
+      title: "Gallery",
+    },
+    {
+      path: "/branches",
+      title: "Branches",
+    },
+    {
+      path: "/pharmacy",
+      title: "Pharmacy",
+    },
+    {
+      path: "/newsMedia",
+      title: "News & Media",
+    },
+    {
+      path: "/contactUs",
+      title: "Contact Us",
+    },
+  ];
+
+  return (
+    <header className="">
+      <nav className=" flex justify-between items-center container mx-auto h-24">
+        <div>
+          <Link to="/">
+            <img src={logo} className=" h-14" alt="logo" />
+          </Link>
         </div>
-    );
+        <ul className="flex justify-between items-center gap-10">
+          {navlinks.map((link) => (
+            <li
+              key={link.path}
+              className="  text-textBlack font-medium hover:font-bold hover:text-secondary duration-300"
+            >
+              <NavLink to={link.path}>{link.title}</NavLink>
+            </li>
+          ))}
+        </ul>
+        <div className=" hidden lg:block">
+          <button className=" bg-slate-200 rounded-full px-6 py-3 font-semibold text-textBlack">
+            Login
+          </button>
+        </div>
+      </nav>
+    </header>
+  );
 };
 
 export default Navbar;
