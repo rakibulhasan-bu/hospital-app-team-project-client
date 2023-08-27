b8cc10d6a932de203d190f58ed1a4c08e1877d9b
SumonaShimu <sumona59shimu@gmail.com>
2023-08-27 19:30:57 +0600

    footer fixed

--- src/pages/shared/appointmentFooter/AppointmentFooter.tsx
+++ src/pages/shared/appointmentFooter/AppointmentFooter.tsx
@@ -4,19 +4,19 @@ import { Link } from "react-router-dom";
 
 const AppointmentFooter: React.FC = () => {
   return (
-    <div className="fixed bottom-0 left-0 right-0 bg-secondary p-4 h-12 flex justify-evenly items-center shadow-t sm:shadow-md">
+    <div className="fixed bottom-0 left-0 right-0 z-50 flex items-center h-12 p-4 bg-secondary justify-evenly shadow-t sm:shadow-md">
       <Link to="/appointment" className="flex items-center text-white hover:text-black">
-        <FaCalendarAlt className="text-xl mr-1" />
+        <FaCalendarAlt className="mr-1 text-xl" />
         Book Appointment
       </Link>
-      <div className="h-12 bg-white w-px mx-4"></div>
+      <div className="w-px h-12 mx-4 bg-white"></div>
       <Link to="/doctors" className="flex items-center text-white hover:text-black">
-        <FaUserMd className="text-xl mr-1" />
+        <FaUserMd className="mr-1 text-xl" />
         Find a Doctor
       </Link>
-      <div className="h-12 bg-white w-px mx-4"></div>
+      <div className="w-px h-12 mx-4 bg-white"></div>
       <Link to="/packages" className="flex items-center text-white hover:text-black">
-        <FaGift className="text-xl mr-1" />
+        <FaGift className="mr-1 text-xl" />
         Packages
       </Link>
     </div>
