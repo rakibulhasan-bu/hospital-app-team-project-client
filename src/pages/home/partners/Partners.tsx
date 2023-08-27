d066e50e5797a3a15be90915d0160cd3d5b2b29d
sifat <md.sifat.ur.rahman2702@gmail.com>
2023-08-27 08:47:39 +0600

    add_title

--- src/pages/home/partners/Partners.tsx
+++ src/pages/home/partners/Partners.tsx
@@ -1,7 +1,19 @@
+import { FaCircle } from "react-icons/fa";
+
 const Partners = () => {
     return (
-        <div className=''>
-            Partners
+        <div className="container px-2 mx-auto my-10 text-xl lg:min-h-screen lg:px-0">
+            <div>
+            <h1 className="flex items-center justify-center gap-1 mb-5 text-2xl font-semibold text-secondary/90">
+              <FaCircle className="text-xs text-accent" />
+              <FaCircle className="text-xl" />
+              <FaCircle className="text-xs text-accent" />
+              <span className="mx-1 text-3xl">Browse by Specialities</span>
+              <FaCircle className="text-xs text-accent" />
+              <FaCircle className="text-xl" />
+              <FaCircle className="text-xs text-accent" />
+            </h1>
+            </div>
         </div>
     );
 };
