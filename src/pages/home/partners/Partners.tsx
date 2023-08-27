99d617f515fba4e13fc4e20f8d3e97f1a63f1035
sifat <md.sifat.ur.rahman2702@gmail.com>
2023-08-27 08:59:06 +0600

    add_title_style

--- src/pages/home/partners/Partners.tsx
+++ src/pages/home/partners/Partners.tsx
@@ -2,9 +2,10 @@ import { FaCircle } from "react-icons/fa";
 
 const Partners = () => {
     return (
-        <div className="container px-2 mx-auto my-10 text-xl lg:min-h-screen lg:px-0">
-            <div>
-            <h1 className="flex items-center justify-center gap-1 mb-5 text-2xl font-semibold text-secondary/90">
+        <div className="container px-2 mx-auto my-10 text-xl lg:min-h-screen lg:px-0 bg-sky-100">
+            <section >
+            <div className="text-center pt-12">
+            <h1 className="flex items-center justify-center gap-1 mb-5 text-2xl font-semibold text-secondary/90 ">
               <FaCircle className="text-xs text-accent" />
               <FaCircle className="text-xl" />
               <FaCircle className="text-xs text-accent" />
@@ -13,7 +14,10 @@ const Partners = () => {
               <FaCircle className="text-xl" />
               <FaCircle className="text-xs text-accent" />
             </h1>
+            <p className="text-slate-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod</p>
+            <p className="text-slate-500">tempor incididunt ut labore et dolore magna aliqua.</p>
             </div>
+            </section>
         </div>
     );
 };
