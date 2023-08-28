216a4084d42893a9688c0fee3254171544965af9
Shaikat roy <shaikatroy258@gmail.com>
2023-08-28 22:50:43 +0600

    pharmacy home started

---
+++ src/pages/home/pharmacyHome/PharmacyCard.tsx
@@ -0,0 +1,12 @@
+
+
+const PharmacyCard = () => {
+    return (
+        <div className="m-4 w-64 text-center">
+        <img src="https://doccure-laravel.dreamguystech.com/template/public/assets/img/doctors/doctor-13.jpg" alt= "medicine" className="max-w-full h-auto rounded-2xl" />
+        <p className="font-medium mt-4 text-textGray">Medicine</p>
+      </div>
+    );
+};
+
+export default PharmacyCard;
--- src/pages/home/pharmacyHome/PharmacyHome.tsx
+++ src/pages/home/pharmacyHome/PharmacyHome.tsx
@@ -1,7 +1,15 @@
+import SectionHeading from "../../../components/sharedTitle/SectionHeading";
+import PharmacyCard from "./pharmacyCard";
+
 const PharmacyHome = () => {
     return (
         <div className=''>
-            PharmacyHome
+            <SectionHeading
+             semiSubTitle="What out patients says about us"
+             title="Happy Clients"
+             subTitle="Patient Voices : A Glimpse into LifeCare Hospital Through the Eyes of Those We've Cared For."
+            />
+            <PharmacyCard></PharmacyCard>
         </div>
     );
 };
