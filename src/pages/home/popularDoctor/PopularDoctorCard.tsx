df9e3ddf5b0ca1f26c240a0270ea3cca3b81bb7a
Shafaet-j <lbshaft01@gmail.com>
2023-08-29 00:09:29 +0600

    register page

--- src/pages/home/popularDoctor/PopularDoctorCard.tsx
+++ src/pages/home/popularDoctor/PopularDoctorCard.tsx
@@ -13,7 +13,7 @@ interface PopularDoctorCardProps {
 const PopularDoctorCard: React.FC<PopularDoctorCardProps> = ({ card }) => {
   return (
     <div className="hover:bg-gradient-to-b from-cyan-300 to-transparent p-[2px] rounded-3xl">
-      <div className=" bg-white rounded-3xl p-6 flex flex-col gap-4">
+      <div className=" bg-white rounded-3xl p-6 flex flex-col gap-4 mb-10">
         <img className=" rounded-3xl" src={card.img} alt="" />
         <div className=" flex items-center gap-3">
           <span className=" bg-green-200 px-3 py-1 text-green-600">
