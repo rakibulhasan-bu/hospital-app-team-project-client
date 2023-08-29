ccc159380dc7ff83bddb096acfd7618b1604dc10
Shaikat roy <shaikatroy258@gmail.com>
2023-08-29 19:39:26 +0600

    about us added

--- src/pages/home/aboutUsHome/AboutUsHome.tsx
+++ src/pages/home/aboutUsHome/AboutUsHome.tsx
@@ -1,8 +1,37 @@
 const AboutUsHome = () => {
     return (
-        <div className=''>
-            AboutUsHome
-        </div>
+        <section className='flex'>
+                <div className="">
+                    <img src="https://i.ibb.co/3FKGpDW/pexels-tima-miroshnichenko-5452293.jpg" alt="" className=""/>
+                    <div>set this div position on the image</div>
+                </div>
+                <div>
+                    <p>Welcome To Best Medical & Health Care</p>
+                    <h1>Get Best & Amazing Experice With Our Professional Doctors</h1>
+                    <p>Meddox Are A Medical And Health Department Provider Institutions. Suitable For Healthcare, Medical, Doctor, Dental, Dentist, Pharmacy, Health And Any Related Medical Care Field.</p>
+                <div className="flex">
+                    <div>
+                        <p>Professional Doctors</p>
+                        <p>Professional Doctors</p>
+                        <p>Professional Doctors</p>
+                        <p>Professional Doctors</p>
+                    </div>
+                    <div>
+                        <p>Professional Doctors</p>
+                        <p>Professional Doctors</p>
+                        <p>Professional Doctors</p>
+                        <p>Professional Doctors</p>
+                    </div>
+                </div>
+                <div className="flex">
+                    <img src="" alt="img" />
+                    <div>
+                        <p>Dr Anwar Ramadan</p>
+                        <p>Executive Director</p>
+                    </div>
+                </div>
+                </div>
+        </section>
     );
 };
 
