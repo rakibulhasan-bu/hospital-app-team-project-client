1f6917bd7fcbbc89122ac373163970b9230f2f53
Shaikat roy <shaikatroy258@gmail.com>
2023-08-29 16:46:34 +0600

    pharmecy card added

--- src/pages/home/pharmacyHome/PharmacyCard.tsx
+++ src/pages/home/pharmacyHome/PharmacyCard.tsx
@@ -1,10 +1,17 @@
+interface Card {
+  img: string;
+  title: string;
+}
 
+interface PharmacyCardProps {
+  card: Card;
+}
 
-const PharmacyCard = () => {
+const PharmacyCard: React.FC<PharmacyCardProps> = ({ card }) => {
     return (
         <div className="m-4 w-64 text-center">
-        <img src="https://doccure-laravel.dreamguystech.com/template/public/assets/img/doctors/doctor-13.jpg" alt= "medicine" className="max-w-full h-auto rounded-2xl" />
-        <p className="font-medium mt-4 text-textGray">Medicine</p>
+        <img src={card.img} alt= "medicine" className="max-w-full h-auto rounded-2xl" />
+        <p className="font-medium mt-4 text-textGray">{card.title}</p>
       </div>
     );
 };
