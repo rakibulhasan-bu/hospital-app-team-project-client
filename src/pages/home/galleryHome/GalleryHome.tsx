36749b46d7b51edf5e8e6fcd240c0eb83684fc9a
Shaikat roy <shaikatroy258@gmail.com>
2023-08-31 00:00:19 +0600

    grid gallery added

--- src/pages/home/galleryHome/GalleryHome.tsx
+++ src/pages/home/galleryHome/GalleryHome.tsx
@@ -1,83 +1,91 @@
 import React from "react";
-import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
 import SectionHeading from "../../../components/sharedTitle/SectionHeading";
-
-const images: { url: string; width: number; height: number }[] = [
-    {
-        url: "https://i.ibb.co/0r9cG8r/pexels-photo-247786.jpg",
-        width: 400,
-        height: 200,
-    },
-    {
-        url: "https://i.ibb.co/0r9cG8r/pexels-photo-247786.jpg",
-        width: 300,
-        height: 450,
-    },
-    {
-        url: "https://i.ibb.co/0r9cG8r/pexels-photo-247786.jpg",
-        width: 200,
-        height: 300,
-    },
-    {
-        url: "https://i.ibb.co/0r9cG8r/pexels-photo-247786.jpg",
-        width: 400,
-        height: 400,
-    },
-    {
-        url: "https://i.ibb.co/0r9cG8r/pexels-photo-247786.jpg",
-        width: 250,
-        height: 350,
-    },
-    {
-        url: "https://i.ibb.co/0r9cG8r/pexels-photo-247786.jpg",
-        width: 350,
-        height: 200,
-    },
-    {
-        url: "https://i.ibb.co/0r9cG8r/pexels-photo-247786.jpg",
-        width: 200,
-        height: 300,
-    },
-    {
-        url: "https://i.ibb.co/0r9cG8r/pexels-photo-247786.jpg",
-        width: 400,
-        height: 200,
-    },
-    {
-        url: "https://i.ibb.co/0r9cG8r/pexels-photo-247786.jpg",
-        width: 200,
-        height: 150,
-    },
-    {
-        url: "https://i.ibb.co/0r9cG8r/pexels-photo-247786.jpg",
-        width: 200,
-        height: 350,
-    },
-];
 
 const GalleryHome: React.FC = () => {
     return (
+
         <>
             <SectionHeading
                 semiSubTitle="What product do we have"
-                title="Photo Gallery"
-                subTitle="Check our pharmacy for your medicine and product"
+                title="Hospital Gallery"
+                subTitle="At Lifecare Hospital. We have been actively revamping our technologies with the most modern one since the first day of our journey.All the aged machineries are being regularly replaced with the latest one. Technical team members are maintaining the machineries actively so that these can provide the best outcome."
             />
-            <ResponsiveMasonry columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3 }}>
-                <Masonry>
-                    {images.map((image, i) => (
-                        <img
-                            key={i}
-                            src={image.url}
-                            width={image.width}
-                            height={image.height}
-                            style={{ display: "block" }}
-                            alt=""
-                        />
-                    ))}
-                </Masonry>
-            </ResponsiveMasonry>
+            <div className="container mx-auto grid grid-cols-8 grid-rows-8 gap-4 my-10">
+                <div className="rounded overflow-hidden shadow-md col-span-4 row-span-4">
+                    <img
+                        src="https://i.ibb.co/0r9cG8r/pexels-photo-247786.jpg"
+                        alt=""
+                        className="w-full h-full object-cover"
+                    />
+                </div>
+                <div className="rounded overflow-hidden shadow-md col-span-2 row-span-2">
+                    <img
+                        src="https://i.ibb.co/0r9cG8r/pexels-photo-247786.jpg"
+                        alt=""
+                        className="w-full h-full object-cover"
+                    />
+                </div>
+                <div className="rounded overflow-hidden shadow-md col-span-2 row-span-2">
+                    <img
+                        src="https://i.ibb.co/0r9cG8r/pexels-photo-247786.jpg"
+                        alt=""
+                        className="w-full h-full object-cover"
+                    />
+                </div>
+                <div className="rounded overflow-hidden shadow-md col-span-2 row-span-2">
+                    <img
+                        src="https://i.ibb.co/0r9cG8r/pexels-photo-247786.jpg"
+                        alt=""
+                        className="w-full h-full object-cover"
+                    />
+                </div>
+                <div className="rounded overflow-hidden shadow-md col-span-2 row-span-2">
+                    <img
+                        src="https://i.ibb.co/0r9cG8r/pexels-photo-247786.jpg"
+                        alt=""
+                        className="w-full h-full object-cover"
+                    />
+                </div>
+                <div className="rounded overflow-hidden shadow-md col-span-2 row-span-2">
+                    <img
+                        src="https://i.ibb.co/0r9cG8r/pexels-photo-247786.jpg"
+                        alt=""
+                        className="w-full h-full object-cover"
+                    />
+                </div>
+                <div className="rounded overflow-hidden shadow-md col-span-2 row-span-2">
+                    <img
+                        src="https://i.ibb.co/0r9cG8r/pexels-photo-247786.jpg"
+                        alt=""
+                        className="w-full h-full object-cover"
+                    />
+                </div>
+
+                <div className="rounded overflow-hidden shadow-md col-span-4 row-span-4 col-end-9 row-end-9">
+                    <img
+                        src="https://i.ibb.co/0r9cG8r/pexels-photo-247786.jpg"
+                        alt=""
+                        className="w-full h-full object-cover"
+                    />
+                </div>
+                <div className="rounded overflow-hidden shadow-md col-span-2 row-span-2">
+                    <img
+                        src="https://i.ibb.co/0r9cG8r/pexels-photo-247786.jpg"
+                        alt=""
+                        className="w-full h-full object-cover"
+                    />
+                </div>
+                <div className="rounded overflow-hidden shadow-md col-span-2 row-span-2">
+                    <img
+                        src="https://i.ibb.co/0r9cG8r/pexels-photo-247786.jpg"
+                        alt=""
+                        className="w-full h-full object-cover"
+                    />
+                </div>
+
+            </div>
         </>
+
     );
 };
 
