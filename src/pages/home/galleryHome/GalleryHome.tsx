77f341a0bc4d84824107558dd395db999a2b2b7c
Shaikat roy <shaikatroy258@gmail.com>
2023-08-30 19:33:17 +0600

    need to size image

--- src/pages/home/galleryHome/GalleryHome.tsx
+++ src/pages/home/galleryHome/GalleryHome.tsx
@@ -1,40 +1,84 @@
 import React from "react";
 import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
+import SectionHeading from "../../../components/sharedTitle/SectionHeading";
 
 const images: { url: string; width: number; height: number }[] = [
-  {
-    url: "https://i.ibb.co/0r9cG8r/pexels-photo-247786.jpg",
-    width: 400,
-    height: 200,
-  },
-  {
-    url: "https://i.ibb.co/0r9cG8r/pexels-photo-247786.jpg",
-    width: 200,
-    height: 300,
-  },
-  {
-    url: "https://i.ibb.co/0r9cG8r/pexels-photo-247786.jpg",
-    width: 300,
-    height: 500,
-  },
-  // ... add more images with different dimensions
+    {
+        url: "https://i.ibb.co/0r9cG8r/pexels-photo-247786.jpg",
+        width: 400,
+        height: 200,
+    },
+    {
+        url: "https://i.ibb.co/0r9cG8r/pexels-photo-247786.jpg",
+        width: 300,
+        height: 450,
+    },
+    {
+        url: "https://i.ibb.co/0r9cG8r/pexels-photo-247786.jpg",
+        width: 200,
+        height: 300,
+    },
+    {
+        url: "https://i.ibb.co/0r9cG8r/pexels-photo-247786.jpg",
+        width: 400,
+        height: 400,
+    },
+    {
+        url: "https://i.ibb.co/0r9cG8r/pexels-photo-247786.jpg",
+        width: 250,
+        height: 350,
+    },
+    {
+        url: "https://i.ibb.co/0r9cG8r/pexels-photo-247786.jpg",
+        width: 350,
+        height: 200,
+    },
+    {
+        url: "https://i.ibb.co/0r9cG8r/pexels-photo-247786.jpg",
+        width: 200,
+        height: 300,
+    },
+    {
+        url: "https://i.ibb.co/0r9cG8r/pexels-photo-247786.jpg",
+        width: 400,
+        height: 200,
+    },
+    {
+        url: "https://i.ibb.co/0r9cG8r/pexels-photo-247786.jpg",
+        width: 200,
+        height: 150,
+    },
+    {
+        url: "https://i.ibb.co/0r9cG8r/pexels-photo-247786.jpg",
+        width: 200,
+        height: 350,
+    },
 ];
 
 const GalleryHome: React.FC = () => {
-  return (
-    <ResponsiveMasonry columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3 }}>
-      <Masonry>
-        {images.map((image, i) => (
-          <img
-            key={i}
-            src={`${image.url}/${image.width}/${image.height}`}
-            style={{ width: "100%", display: "block" }}
-            alt=""
-          />
-        ))}
-      </Masonry>
-    </ResponsiveMasonry>
-  );
+    return (
+        <>
+            <SectionHeading
+                semiSubTitle="What product do we have"
+                title="Photo Gallery"
+                subTitle="Check our pharmacy for your medicine and product"
+            />
+            <ResponsiveMasonry columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3 }}>
+                <Masonry>
+                    {images.map((image, i) => (
+                        <img
+                            key={i}
+                            src={image.url}
+                            width={image.width}
+                            height={image.height}
+                            style={{ display: "block" }}
+                            alt=""
+                        />
+                    ))}
+                </Masonry>
+            </ResponsiveMasonry>
+        </>
+    );
 };
 
 export default GalleryHome;
