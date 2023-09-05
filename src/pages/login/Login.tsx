8d0f8ff4a37f8302380d3aabb5cb19780e575300
Rakibul Hasan <rakibulhasanbu@gmail.com>
2023-09-05 18:27:22 +0600

    register page created

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -5,11 +5,13 @@ import { FaGoogle } from "react-icons/fa";
 
 const Login = () => {
   return (
-    <section className=" bg-[#edf0ff] w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 2xl:grid-cols-2 lg:gap-20 2xl:pr-20 lg:pr-20 min-h-[70vh]">
-      <div className=" bg-secondary rounded-r-[50px] hidden lg:block 2xl:block">
-        <img src={design} alt="" />
+    <section className="bg-background w-full h-[82dvh] grid grid-cols-1 md:grid-cols-2">
+      {/* these is image section  */}
+      <div className="bg-secondary w-full h-full rounded-r-[50px] hidden lg:block 2xl:block">
+        <img className="w-full h-full object-contain" src={design} alt="" />
       </div>
-      <div className=" self-center bg-white rounded-3xl p-10 space-y-5">
+      {/* these is form section  */}
+      <div className="self-center mx-20 bg-white rounded-3xl p-10 space-y-5">
         <img className=" h-16 hidden lg:block" src={logo} alt="" />
         <h2 className=" title text-2xl">Getting Started</h2>
         <form className=" space-y-5">
