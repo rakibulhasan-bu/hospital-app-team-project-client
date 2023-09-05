e85674ffd9b0a0e1f64f0e45df633c7832796cea
Rakibul Hasan <rakibulhasanbu@gmail.com>
2023-09-05 19:15:32 +0600

    firebase inialize

---
+++ src/firebase/firebase.config.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAD_oI0Zkl8b05p7cQt1WipmxyWEuXGhxA",
+  authDomain: "lifecare-94133.firebaseapp.com",
+  projectId: "lifecare-94133",
+  storageBucket: "lifecare-94133.appspot.com",
+  messagingSenderId: "438689915589",
+  appId: "1:438689915589:web:805e65e1a6d50220b1c6ac",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
