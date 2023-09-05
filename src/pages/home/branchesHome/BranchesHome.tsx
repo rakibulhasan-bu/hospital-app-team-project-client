657070660896cd374b0c78363f2c08cc40872dad
SumonaShimu <sumona59shimu@gmail.com>
2023-09-05 19:00:35 +0600

    new branches img added

--- src/pages/home/branchesHome/BranchesHome.tsx
+++ src/pages/home/branchesHome/BranchesHome.tsx
@@ -1,21 +1,21 @@
 import SectionHeading from "../../../components/sharedTitle/SectionHeading";
-
+import image from "../../../assets/v3.png";
 const BranchesHome = () => {
   return (
-    <div className="bg-[#efefef] max-h-100vh pt-10 pb-5 w-full">
-      <SectionHeading 
+    <div className="w-full pt-10 pb-5 max-h-100vh">
+      {/* <SectionHeading 
       title="Our branches"
-      semiSubTitle="Serving Communities Across 8 Divisions Nationwide"
+      semiSubTitle="Serving Communities Across Nationwide"
       subTitle="
       Lifecare's Nationwide Hospital Network: 25+ Branches, 8 Divisions, One Mission - Your Health."
-      />
-      <img
-        src="https://res.cloudinary.com/dth0nno0p/image/upload/v1693736755/map_xt64sf.png"
-        alt="branches-map"
-        width={''}
-        className="w-1/2"
-      />
-   
+      /> */}
+      <img src={image} alt="branches-map" width={""} className="z-0 w-full" />
+      <div className="h-40 border border-orange-600 w-96">
+        <div>
+
+        </div>
+        <img src="https://res.cloudinary.com/dth0nno0p/image/upload/v1693534293/hospital_kisskz.jpg" alt="" className="w-40 h-40"/>
+      </div>
     </div>
   );
 };
