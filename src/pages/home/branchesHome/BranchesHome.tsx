f287b36f51308249294dc1e4b70a0f57ad3720ca
SumonaShimu <sumona59shimu@gmail.com>
2023-09-05 20:05:07 +0600

    home brances cards added

--- src/pages/home/branchesHome/BranchesHome.tsx
+++ src/pages/home/branchesHome/BranchesHome.tsx
@@ -1,20 +1,23 @@
-import SectionHeading from "../../../components/sharedTitle/SectionHeading";
 import image from "../../../assets/v3.png";
+import BranchesCard from "../../../components/Banches/BranchesCard";
 const BranchesHome = () => {
   return (
     <div className="w-full pt-10 pb-5 max-h-100vh">
-      {/* <SectionHeading 
-      title="Our branches"
-      semiSubTitle="Serving Communities Across Nationwide"
-      subTitle="
-      Lifecare's Nationwide Hospital Network: 25+ Branches, 8 Divisions, One Mission - Your Health."
-      /> */}
+      {/* background image */}
       <img src={image} alt="branches-map" width={""} className="z-0 w-full" />
-      <div className="h-40 border border-orange-600 w-96">
-        <div>
-
+      {/* upper content div */}
+      <div className="ml-auto w-max">
+        {/* <SectionHeading
+            title="Our branches"
+            semiSubTitle="Serving Communities Across Nationwide"
+            subTitle="Lifecare's Nationwide Hospital Network: 25+ Branches, 8 Divisions, One Mission - Your Health."
+          /> */}
+        <BranchesCard />
+        <div className="flex gap-4 my-4 rounded-lg">
+          <BranchesCard />
+          <BranchesCard />
         </div>
-        <img src="https://res.cloudinary.com/dth0nno0p/image/upload/v1693534293/hospital_kisskz.jpg" alt="" className="w-40 h-40"/>
+        <BranchesCard />
       </div>
     </div>
   );
