501dcda5248c9fe69d6c963d53ca061733f6f426
sifat <md.sifat.ur.rahman2702@gmail.com>
2023-09-05 19:00:51 +0600

    add-input

--- src/pages/appointment/AppointmentBanner/AppointmentBanner.tsx
+++ src/pages/appointment/AppointmentBanner/AppointmentBanner.tsx
@@ -1,6 +1,8 @@
 import { format } from 'date-fns';
 import { DayPicker } from 'react-day-picker';
 import 'react-day-picker/dist/style.css'
+
+
 interface AppointmentBannerProps {
   selectedDate: Date;
   setSelectedDate: (date: Date) => void; 
@@ -43,6 +45,17 @@ const AppointmentBanner: React.FC<AppointmentBannerProps> = ({ selectedDate, set
           <div className='bg-sky-100 p-3 mb-6 text-xl rounded-lg'>
             <p className='font-bold'>Patient Email:</p>
             <p>rahman@gmail.com </p>
+          </div>
+          <div className='bg-sky-100 p-3 mb-6 text-xl rounded-lg'>
+          <div className="  relative h-11 w-full min-w-[200px]">
+    <input
+      className="peer h-full w-full border-b border-blue-gray-200 bg-transparent pt-4 pb-1.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border-blue-gray-200 focus:border-slate-900 focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
+      placeholder=" "
+    />
+    <label className="after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-500 transition-all after:absolute after:-bottom-1.5 after:block after:w-full after:scale-x-0 after:border-b-2 after:border-pink-500 after:transition-transform after:duration-300 peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.25] peer-placeholder-shown:text-blue-gray-500 peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-pink-500 peer-focus:after:scale-x-100 peer-focus:after:border-pink-500 peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+      Standard
+    </label>
+          </div>
           </div>
         </div>
         {/* Doctor */}
