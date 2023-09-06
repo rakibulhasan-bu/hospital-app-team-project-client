98394f3226cbd2cd94fb885a9a2515fadcc01f84
sifat <md.sifat.ur.rahman2702@gmail.com>
2023-09-06 09:13:05 +0600

    add-phone

--- src/pages/appointment/Appointment/Appointment.tsx
+++ src/pages/appointment/Appointment/Appointment.tsx
@@ -121,8 +121,16 @@ const Appointment = () => {
           </div>
 
           <div className='bg-sky-100 p-3  text-xl rounded-lg'>
-            <p className='font-bold'>Patient Phone Number:</p>
-            <p>Mr. Rahman </p>
+          <p className=' font-semibold mb-2'>Appointment Time</p>
+          <div className="  relative h-11 w-full min-w-[200px]">
+    <input
+      className="peer h-full w-full border-b border-black bg-transparent pt-4 pb-1.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border-blue-600 focus:border-slate-900 focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
+      placeholder=" "
+    />
+    <label className="after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-500 transition-all after:absolute after:-bottom-1.5 after:block after:w-full after:scale-x-0 after:border-b-2 after:border-black after:transition-transform after:duration-300 peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.25] peer-placeholder-shown:text-blue-gray-500 peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-black peer-focus:after:scale-x-100 peer-focus:after:border-black peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+    Your phone number
+    </label>
+          </div>
           </div>
 
           </div>
