14d5d8cf555d6039dfc1fe5a172658e00f4b8ec8
Rakibul Hasan <rakibulhasanbu@gmail.com>
2023-09-14 19:35:14 +0600

    added next ui

--- src/main.tsx
+++ src/main.tsx
@@ -8,14 +8,17 @@ import './i18n'
 import { Provider } from 'react-redux'
 import Loading from './pages/loading/Loading';
 import { store } from './redux/store';
+import { NextUIProvider } from "@nextui-org/react";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Suspense fallback={<Loading />}>
-      <Provider store={store}>
-        <Toaster position="top-right" />
-        <RouterProvider router={router} />
-      </Provider>
-    </Suspense>
+    <NextUIProvider>
+      <Suspense fallback={<Loading />}>
+        <Provider store={store}>
+          <Toaster position="top-right" />
+          <RouterProvider router={router} />
+        </Provider>
+      </Suspense>
+    </NextUIProvider>
   </React.StrictMode>,
 )
