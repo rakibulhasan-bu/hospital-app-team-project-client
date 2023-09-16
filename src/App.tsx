6876d4564e3417f845af43153c901b6c695eda6f
Rakibul Hasan <rakibulhasanbu@gmail.com>
2023-09-16 13:13:11 +0600

    changes all

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import AppointmentFooter from "./pages/shared/appointmentFooter/AppointmentFooter";
 import Footer from "./pages/shared/footer/Footer";
 import Navbar from "./pages/shared/navbar/Navbar";
-import { Outlet } from "react-router-dom";
+import { Outlet, ScrollRestoration } from "react-router-dom";
+import { RootState } from "./redux/store";
+import { useSelector } from "react-redux";
 
 const App = () => {
+  const { theme } = useSelector((state: RootState) => state.themeState)
   return (
-    <div className=''>
+    <div className={`${theme} bg-background`}>
       <Navbar />
       <Outlet />
       <Footer />
       <AppointmentFooter />
+      <ScrollRestoration />
     </div>
   );
 };
