c12d771d8a05d153bae49ee6c9f2a07037b860cb
SumonaShimu <sumona59shimu@gmail.com>
2023-10-30 00:20:25 +0600

    branches cards with json added

--- src/components/Banches/BranchesCard.tsx
+++ src/components/Banches/BranchesCard.tsx
@@ -1,26 +1,27 @@
-
 import { FaPhoneAlt } from "react-icons/fa";
 import { FaLocationDot, FaRegPaperPlane } from "react-icons/fa6";
 
-const BranchesCard = () => {
+const BranchesCard = ({
+  location = "Sadar, Gazipur",
+  contact = "01675543210",
+  email = "gazipur@lifecare.com",
+  name = "Lifecare Hospital Gazipur",
+}) => {
   return (
-    <div className="h-32 mx-auto overflow-hidden border rounded-lg branches-card border-accent w-max hover:shadow-lg bg-[#112b4ed8] md:bg-transparent">
+    <div className="h-32 w-max mx-auto overflow-hidden border rounded-lg branches-card border-accent hover:shadow-lg bg-[#112b4ed8] md:bg-transparent">
       <div className="flex items-center text-xs font-semibold text-accent">
         <div className="m-3">
           <a href="#">
-            <FaLocationDot className="inline text-secondary" /> Sadar, Gazipur
+            <FaLocationDot className="inline text-secondary" /> {location}
           </a>
           <h3 className="">
             <FaPhoneAlt className="inline text-secondary" /> Contact:
-            01675543210
+            {contact}
           </h3>
           <h3 className="">
-            <FaRegPaperPlane className="inline text-secondary" />{" "}
-            gazipur@lifecare.com
+            <FaRegPaperPlane className="inline text-secondary" /> {email}
           </h3>
-          <h1 className="mt-2 font-bold text-secondary">
-            Lifecare Hospital Gazipur
-          </h1>
+          <h1 className="mt-2 font-bold text-secondary">{name}</h1>
         </div>
         <img
           src="https://res.cloudinary.com/dth0nno0p/image/upload/v1693534293/hospital_kisskz.jpg"
--- src/pages/branches/Branches.tsx
+++ src/pages/branches/Branches.tsx
@@ -1,11 +1,25 @@
-import { Tooltip } from 'react-tooltip'
+import BranchesCard from "../../components/Banches/BranchesCard";
+import allBranches from "../../components/data/branches.json";
+import SectionHeading from "../../components/sharedTitle/SectionHeading";
 
 const Branches = () => {
-    return (
-        <>
-        <h1>branches page</h1>
-      </>
-    );
+  console.log(allBranches);
+  return (
+    <>
+      <SectionHeading title="Our Branches" />
+      <div className="grid grid-cols-1 gap-4 my-10 md:grid-cols-2 lg:grid-cols-4 min-w-max">
+        {allBranches.map((branch) => (
+          <BranchesCard
+            key={branch.name}
+            name={branch.name}
+            location={branch.location}
+            contact={branch.contact}
+            email={branch.email}
+          />
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default Branches;
